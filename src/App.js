7769e7946953ffb41a66e12718ad087dc2efd866
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2023-12-12 17:55:30 -0500

    comment out

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import Modal from './components/Modal';
 
 const url = `http://ddragon.leagueoflegends.com/cdn/12.22.1/data/en_US/champion.json`;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
